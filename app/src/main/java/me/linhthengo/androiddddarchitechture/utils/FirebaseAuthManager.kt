ca448e2ad355f167e43d0cc88909bd4b9f7adc7f
linhngo <thlinh.ngo@gmail.com>
2020-03-20 17:05:16 +0700

    Add Firebase Authentication Manager

---
+++ app/src/main/java/me/linhthengo/androiddddarchitechture/utils/FirebaseAuthManager.kt
@@ -0,0 +1,21 @@
+package me.linhthengo.androiddddarchitechture.utils
+
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+
+class FirebaseAuthManager {
+    private var auth: FirebaseAuth = FirebaseAuth.getInstance()
+
+    fun getCurrentUser() = auth.currentUser
+
+    fun getUpdateUser(user: FirebaseUser) = auth.updateCurrentUser(user)
+
+    fun signOut() = auth.signOut()
+
+    fun register(email: String, password: String) =
+        auth.createUserWithEmailAndPassword(email, password)
+
+    fun signInWithEmail(email: String, password: String) =
+        auth.signInWithEmailAndPassword(email, password)
+
+}
