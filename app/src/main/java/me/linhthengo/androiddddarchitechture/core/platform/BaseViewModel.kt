d149984f5e729bcf6bcc4538c0ffd889da3a23ae
Linh Ngo <thlinh.ngo@gmail.com>
2020-03-24 23:52:47 +0700

    Improve sign in flow

--- app/src/main/java/me/linhthengo/androiddddarchitechture/core/platform/BaseViewModel.kt
+++ app/src/main/java/me/linhthengo/androiddddarchitechture/core/platform/BaseViewModel.kt
@@ -1,15 +1,5 @@
 package me.linhthengo.androiddddarchitechture.core.platform
 
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import me.linhthengo.androiddddarchitechture.core.Failure
-import timber.log.Timber
 
-abstract class BaseViewModel : ViewModel() {
-    var failure: MutableLiveData<Failure> = MutableLiveData()
-
-    protected fun handleFailure(failure: Failure) {
-        Timber.tag(this::class.java.name).e(failure.message)
-        this.failure.value = failure
-    }
-}
+abstract class BaseViewModel : ViewModel()
--- app/src/main/java/me/linhthengo/androiddddarchitechture/presentation/auth/signin/SignInFragment.kt
+++ app/src/main/java/me/linhthengo/androiddddarchitechture/presentation/auth/signin/SignInFragment.kt
@@ -2,19 +2,23 @@ package me.linhthengo.androiddddarchitechture.presentation.auth.signin
 
 import android.os.Bundle
 import android.view.View
+import androidx.core.content.ContextCompat.getDrawable
+import androidx.core.graphics.drawable.toBitmap
 import androidx.core.widget.doAfterTextChanged
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.LifecycleOwner
 import androidx.lifecycle.Observer
+import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import kotlinx.android.synthetic.main.sign_in_fragment.*
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 import me.linhthengo.androiddddarchitechture.R
 import me.linhthengo.androiddddarchitechture.core.platform.BaseFragment
 import me.linhthengo.androiddddarchitechture.domain.auth.EmailAddress
 import me.linhthengo.androiddddarchitechture.domain.auth.Password
 import me.linhthengo.androiddddarchitechture.domain.core.ValueFailures
-import timber.log.Timber
 
 class SignInFragment : BaseFragment() {
     override fun layoutId(): Int = R.layout.sign_in_fragment
@@ -39,45 +43,65 @@ class SignInFragment : BaseFragment() {
         }
 
         btn_sign_in.setOnClickListener {
-            Timber.tag(this::class.java.name).e("sign in clicked")
             if (tv_email.editText!!.error.isNullOrBlank() && tv_password.editText!!.error.isNullOrBlank()) {
-                Timber.tag(this::class.java.name).e("sign in")
                 signInViewModel.signIn(
                     tv_email.editText?.text.toString(),
                     tv_password.editText?.text.toString()
-                ) {
-                    findNavController().navigate(R.id.action_signInFragment_to_homeFragment)
-                }
+                )
             } else {
                 handleEmail(tv_email.editText?.text.toString())
                 handlePassword(tv_password.editText?.text.toString())
             }
         }
 
-        signInViewModel.failure.observe(this.activity as LifecycleOwner, Observer {
-            it?.let {
-                MaterialAlertDialogBuilder(context)
-                    .setTitle(getString(R.string.sign_in_failure))
-                    .setMessage(getString(R.string.error_s).format(it.message))
-                    .setPositiveButton(getString(R.string.close), null)
-                    .show()
-            }
+        signInViewModel.state.observe(this.activity as LifecycleOwner, Observer {
+            it?.let { handleSignIn(it) }
         })
     }
 
-    private fun handleError(valueFailures: ValueFailures<String>) = when (valueFailures) {
+    private fun handleSignIn(state: SignInViewModel.State) {
+        when (state) {
+            is SignInViewModel.State.SignInFailure -> {
+                MaterialAlertDialogBuilder(context)
+                    .setTitle(R.string.sign_in_failure)
+                    .setMessage(getString(R.string.error_s).format(state.message))
+                    .setPositiveButton(R.string.close) { _, _ ->
+                        btn_sign_in.revertAnimation()
+                    }
+                    .show()
+            }
+            is SignInViewModel.State.SignInSuccess -> {
+                lifecycleScope.launch {
+                    btn_sign_in.doneLoadingAnimation(
+                        R.color.white,
+                        getDrawable(
+                            this@SignInFragment.requireContext(),
+                            R.drawable.ic_check_white_24dp
+                        )!!.toBitmap()
+                    )
+                    delay(500)
+                    findNavController().navigate(R.id.action_signInFragment_to_homeFragment)
+                }
+            }
+            is SignInViewModel.State.SignInLoading -> {
+                btn_sign_in.startAnimation()
+            }
+        }
+    }
+
+    private fun handleValidatorError(valueFailures: ValueFailures<String>) = when (valueFailures) {
         is ValueFailures.ShortPassword<String> -> getString(R.string.password_too_short)
         is ValueFailures.InvalidEmail -> getString(R.string.invalid_email)
     }
 
     private fun handlePassword(input: String) = Password(input).value.fold({ valueFailures ->
-        tv_password.editText?.error = handleError(valueFailures)
+        tv_password.editText?.error = handleValidatorError(valueFailures)
     }, {
         tv_password.editText?.error = null
     })
 
     private fun handleEmail(input: String) = EmailAddress(input).value.fold({ valueFailures ->
-        tv_email.editText?.error = handleError(valueFailures)
+        tv_email.editText?.error = handleValidatorError(valueFailures)
     }, {
         tv_email.editText?.error = null
     })
